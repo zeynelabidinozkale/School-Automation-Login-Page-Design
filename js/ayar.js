2f72f0d8acfa8dbb452a02eaf1b36a02b1b31757
Zeynel Abidin Özkale <zeynelOzkale@users.noreply.github.com>
2020-01-14 04:57:44 +0300

    Initial Commit

---
+++ js/ayar.js
@@ -0,0 +1,149 @@
+$.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js", function(){
+    particlesJS('particles-js',
+      {
+        "particles": {
+          "number": {
+            "value": 160,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 5,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 3,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 150
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true,
+        "config_demo": {
+          "hide_card": false,
+          "background_color": "#b61924",
+          "background_image": "",
+          "background_position": "50% 50%",
+          "background_repeat": "no-repeat",
+          "background_size": "cover"
+        }
+      }
+    );
+
+});
+
+// https://selimdoyranli.com
+// https://www.RadKod.com
+
+$(document).ready(function(){
+
+//Kaydol - Şifre Unuttum Linkleri Arası Geçiş
+$(document).ready(function(){
+$("#kayit-form").hide();
+$("#sifre-hatirlat-form").hide();
+$(".hesap-olustur-link").click(function(e){
+$("#login-form").slideUp(0);
+$("#kayit-form").fadeIn(300);
+});
+
+$(".zaten-hesap-var-link").click(function(e){
+$("#kayit-form").slideUp(0);
+$("#sifre-hatirlat-form").slideUp(0);
+$("#login-form").fadeIn(300);
+});
+
+$(".sifre-hatirlat-link").click(function(e){
+$("#login-form").slideUp(0);
+$("#sifre-hatirlat-form").fadeIn(300);
+});
+});
+
+});
+//*****************************************************
